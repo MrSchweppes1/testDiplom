df256913d991dde63574005eb9a7426322f4f61c
Василий Воробьев <vvorobiov123@gmail.com>
2022-05-12 16:19:22 +0300

    Осознал бд

--- app/src/main/java/com/example/testdiplom/MainActivity.kt
+++ app/src/main/java/com/example/testdiplom/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.testdiplom
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import com.example.testdiplom.db.MyDbManager
 
 class MainActivity : AppCompatActivity() {
@@ -15,9 +17,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun onClickSave(view: View) {
+    override fun onResume() {
+        super.onResume()
         myDbManager.openDb()
-        myDbManager.insertToDb(edTitle.text.ToString()) // почему то не видит едит текст
+        val dataList = myDbManager.readDbData()
+
+        for (item in dataList){
+            findViewById<TextView>(R.id.tvTest).append(item)
+            findViewById<TextView>(R.id.tvTest).append("\n")
+
+        }
+    }
+
+    fun onClickSave(view: View) {
+
+        findViewById<TextView>(R.id.tvTest).text = ""
+
+        myDbManager.insertToDb(findViewById<EditText>(R.id.edTitle).text.toString()) // почему то не видит едит текст
+       val dataList = myDbManager.readDbData()
+
+        for (item in dataList){
+            findViewById<TextView>(R.id.tvTest).append(item)
+            findViewById<TextView>(R.id.tvTest).append("\n")
+
+        }
+
     }
 
     override fun onDestroy(){
